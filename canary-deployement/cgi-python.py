52712a3082d33cd79f33a1c3e616bb68c6585281
Navaneetkn <kangokarnavaneet@gmail.com>
2021-06-12 14:44:56 +0530

    Project completed

---
+++ canary-deployement/cgi-python.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+print("Content-type:text/html\r\n\r\n")
+print('<html>')
+print('<head>')
+print('<title>Hello SCRIBETECH</title>')
+print('</head>')
+print('<body>')
+print('<h1>Hello SCRIBETECH</h1>')
+print('</body>')
+print('</html>')
